2cde9c871e544815db948b8ed54f8425d48308d2
kajj8808 <a01039388808@gmail.com>
2025-04-29 11:54:45 +0900

    ✅ login 중에 id가 없을경우 log-in 페이지로 넘어가는 부분 추가

--- app/lib/server/auth.ts
+++ app/lib/server/auth.ts
@@ -6,7 +6,7 @@ export async function authWithUserSession() {
   // 로그인 상태 확인
   const userSession = await getUserSession();
   // 로그인되어 있지 않다면 로그인 페이지로 redirect
-  if (!userSession) {
+  if (!userSession.id) {
     return redirect("/log-in");
   } else {
     return userSession;
