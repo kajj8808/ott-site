e5c3cb5103a1cb95611ec8826d87c2d563144b7c
kajj8808 <a01039388808@gmail.com>
2025-06-02 14:06:11 +0900

    🔥 agent 확인용 log 삭제

--- app/lib/server/isBot.ts
+++ app/lib/server/isBot.ts
@@ -5,7 +5,6 @@ import { headers } from "next/headers";
 export async function isBotRequest() {
   const userAgent = (await headers()).get("user-agent");
   const botKeywords = ["discordbot", "slackbot", "kakaotalk"];
-  console.log(userAgent);
 
   const isBot = botKeywords.some((botKeyword) =>
     userAgent?.includes(botKeyword),
