fabf1d9205d561335a0b053af116845118dc056b
kajj8808 <a01039388808@gmail.com>
2025-07-12 16:33:28 +0900

    🐛 업로드 문제 수정1.

--- app/(private)/live/[roomName]/client.tsx
+++ app/(private)/live/[roomName]/client.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import useLiveVideoSync from "@/app/hooks/useLiveVideoSync";
-import { getVideoUrl } from "@/app/utils/libs";
-import { useEffect, useRef } from "react";
+
+import { useEffect, useRef, useState } from "react";
 
 export default function LiveClient({
   roomName,
@@ -21,18 +21,28 @@ export default function LiveClient({
     pauseVideo,
   } = useLiveVideoSync();
 
+  const [isSeekingFromRemote, setIsSeekingFromRemote] = useState(false);
+
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
   useEffect(() => {
     if (roomName) {
-      // joinRoom(roomName, userName);
+      joinRoom(roomName, userName);
     }
   }, []);
 
   useEffect(() => {
     const video = videoRef.current;
     if (video) {
-      //video.addEventListener("")
+      //
+      const seekingVideo = () => {
+        console.log("seeking video time!", video.currentTime);
+      };
+      video.volume = 0.2;
+      video.addEventListener("seek", seekingVideo);
+      return () => {
+        video.removeEventListener("seek", seekingVideo);
+      };
     }
   }, []);
 
@@ -45,6 +55,17 @@ export default function LiveClient({
       <video ref={videoRef} controls crossOrigin="anonymous" autoPlay>
         <source src="https://kajj8808.com:8443/media/video/1752049479794" />
       </video>
+
+      <button
+        onClick={() => {
+          const video = videoRef.current;
+          if (video) {
+            video.currentTime = video.currentTime + 1000;
+          }
+        }}
+      >
+        seeking test
+      </button>
     </div>
   );
 }
